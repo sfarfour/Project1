a1a71f1534e73a0024983a202b0bed834573f83e
Samer Farfour <samerfarfour@hotmail.com>
2023-08-13 16:00:58 -0400

    Name change

---
+++ 01_wd_libraries.R
@@ -0,0 +1,20 @@
+### Working Directory ###
+setwd("C:/Users/samer/OneDrive/Desktop/R/Project1")
+
+
+### Install packages ###
+
+# General
+install.packages("dplyr")
+install.packages("knitr")
+install.packages("tidyverse")
+install.packages("readxl")
+install.packages("haven")
+
+# Graphs
+install.packages("ggplot2")
+install.packages("shiny")
+install.packages("ggthemes")
+
+
+
